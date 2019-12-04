8353b59009ca026e0859949bd67d4f66b5806bf2
Jeffery Gontarek <jeffery@Jefferys-MacBook-Pro.local>
2019-12-03 23:02:16 -0800

    invalid entry inserted completed

--- control_flow_game.php
+++ control_flow_game.php
@@ -10,15 +10,17 @@ while ($end_game != true) {
 	$my_number = rand(0,9);
 	$your_number = rtrim(fgets(STDIN));
 
-	if ($your_number > 9 || $your_number < 0) {
-		echo "Invalid number $your_number. Your number must be between 0 and 9\n";
-}	elseif ($my_number == $your_number) {
-		echo "It's a tie! Play again?\n";
-}	elseif ($my_number < $your_number) {
-		echo "You cheated! Play again.\n";
-}	elseif ($my_number > $your_number) {
-		exit("I won! Bye now.\n");
-}
+		if (is_numeric($your_number) != true ) {
+			echo "Invalid entry. You must enter a number. Your number must be a number between 0 and 9\n";
+	}	elseif ($your_number > 9 || $your_number < 0) {
+			echo "Invalid number $your_number. Your number must be between 0 and 9\n";
+	}	elseif ($my_number == $your_number) {
+			echo "It's a tie! Play again?\n";
+	}	elseif ($my_number < $your_number) {
+			echo "You cheated! Play again.\n";
+	}	elseif ($my_number > $your_number) {
+			exit("I won! Bye now.\n");
+	}
 }
 
 ?>
