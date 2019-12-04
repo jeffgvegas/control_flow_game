839253c613788945072294f615b7c1bb6ae7e261
Jeffery Gontarek <jeffery@Jefferys-MacBook-Pro.local>
2019-12-03 22:10:48 -0800

    pick a number

--- control_flow_game.php
+++ control_flow_game.php
@@ -4,3 +4,11 @@ echo "Let's play a game!\n";
 echo "Higher number wins.\n";
 
 $end_game = false;
+
+while ($end_game != true) {
+	echo "Pick a number between 0 and 9\n";
+	$my_number = rand(0,9);
+	$your_number = rtrim(fgets(STDIN));
+}
+
+?>
